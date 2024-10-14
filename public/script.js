60cf17cda963f762cbcde147a7319f0a5b559390
nitzano707 <nitzan.orot@gmail.com>
2024-10-14 11:53:42 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const input = document.getElementById('input');
 const submit = document.getElementById('submit');
 const output = document.getElementById('output');
 
-const API_URL = 'https://api-inference.huggingface.co/models/google/gemma-2b-it';
+//const API_URL = 'https://api-inference.huggingface.co/models/google/gemma-2b-it';
+const API_URL = 'Nitzantry1/mistralai-Mixtral-8x7B-Instruct-v0.1';
 //let HUGGING_FACE_API_KEY = '{{HUGGING_FACE_API_KEY}}';
 let HUGGING_FACE_API_KEY = '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';
 
